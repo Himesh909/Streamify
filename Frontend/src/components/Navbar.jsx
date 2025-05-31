585843adbcd86c7c2ec4cd3168ae864ba74d72d5
Himesh909 <135736246+Himesh909@users.noreply.github.com>
2025-05-31 17:34:54 +0530

    modified layout

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -11,25 +11,22 @@ const Navbar = () => {
   const { logoutMutation, isPending, error } = useLogout();
 
   return (
-    <nav className="bg-base-200 border-b border-base-300 sticky top-0 z-30 h-16 flex items-center">
+    <nav className="bg-base-200 border-b border-base-300 sticky top-0 z-30 h-16 flex items-center w-full">
+      <div className="pl-5 sm:px-6 lg:px-8">
+        <Link to="/" className="flex items-center gap-2.5">
+          <ShipWheelIcon className="size-6 sm:size-9 text-primary" />
+          <span className="text-xl sm:text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wider">
+            Streamify
+          </span>
+        </Link>
+      </div>
+
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-end w-full">
-          {/* LOGO - ONLY IN THE CHAT PAGE */}
-          {isChatPage && (
-            <div className="pl-5">
-              <Link to="/" className="flex items-center gap-2.5">
-                <ShipWheelIcon className="size-9 text-primary" />
-                <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary  tracking-wider">
-                  Streamify
-                </span>
-              </Link>
-            </div>
-          )}
-
           <div className="flex items-center gap-3 sm:gap-4 ml-auto">
             <Link to={"/notifications"}>
-              <button className="btn btn-ghost btn-circle">
-                <BellIcon className="h-6 w-6 text-base-content opacity-70" />
+              <button className="btn btn-ghost btn-circle btn-sm sm:btn-md">
+                <BellIcon className="h-4 w-4 sm:h-6 sm:w-6 text-base-content opacity-70" />
               </button>
             </Link>
           </div>
@@ -37,7 +34,7 @@ const Navbar = () => {
           <ThemeSelector />
 
           <div className="avatar">
-            <div className="w-9 rounded-full">
+            <div className="w-7 sm:w-9 rounded-full">
               <img
                 src={authUser?.profilePic}
                 alt="User Avatar"
@@ -47,8 +44,11 @@ const Navbar = () => {
           </div>
 
           {/* Logout button */}
-          <button className="btn btn-ghost btn-circle" onClick={logoutMutation}>
-            <LogOutIcon className="h-6 w-6 text-base-content opacity-70" />
+          <button
+            className="btn btn-ghost btn-circle btn-sm sm:btn-md"
+            onClick={logoutMutation}
+          >
+            <LogOutIcon className="h-4 w-4 sm:h-6 sm:w-6 text-base-content opacity-70" />
           </button>
         </div>
       </div>
